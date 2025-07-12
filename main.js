a22b7664b036c3b00781875d7a0117e4dfe2ceb2
sanjograiofficial <sanjograiofficial@gmail.com>
2025-07-12 21:02:50 +0545

    Up Arrow

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const resetBtn = document.querySelector(".reset");
 
 const sec0UpBtn = document.querySelector(".sec0.up");
 const sec1UpBtn = document.querySelector(".sec1.up");
+const min0UpBtn = document.querySelector(".min0.up");
+const min1UpBtn = document.querySelector(".min1.up");
+const h0UpBtn = document.querySelector(".hr0.up");
+const h1UpBtn = document.querySelector(".hr1.up");
 
 let hour = document.querySelector(".hour");
 let minute = document.querySelector(".minute");
@@ -59,10 +63,51 @@ resetBtn.addEventListener("click", () => {
   minute.textContent = `00`;
   second.textContent = `00`;
 });
+h0UpBtn.addEventListener("click", () => {
+  h[0]++;
+  if (h[0] > 2) {
+    h[0] = 0;
+  }
+  hour.textContent = `${h[0]}${h[1]}`;
+});
+h1UpBtn.addEventListener("click", () => {
+  if(hour.textContent == 24) return;
+  h[1]++;
+  if (h[1] > 9) {
+    h[1] = 0;
+    h[0]++;
+  }
+  if (h[0] > 2) {
+    h[0] = 0;
+  }
+  hour.textContent = `${h[0]}${h[1]}`;
+});
 
-sec0UpBtn.addEventListener("click", () => {
-  sec[0]++;
-});
-sec1UpBtn.addEventListener("click", () => {
-  sec[1]++;
-});
+function inc0Btn(btn, time, timeText) {
+  btn.addEventListener("click", () => {
+    time[0]++;
+    if (time[0] > 5) {
+      time[0] = 0;
+    }
+    timeText.textContent = `${time[0]}${time[1]}`;
+  });
+}
+
+function inc1Btn(btn, time, timeText) {
+  btn.addEventListener("click", () => {
+    time[1]++;
+    if (time[1] > 9) {
+      time[1] = 0;
+      time[0]++;
+    }
+    if (time[0] > 5) {
+      time[0] = 0;
+    }
+    timeText.textContent = `${time[0]}${time[1]}`;
+  });
+}
+inc0Btn(sec0UpBtn, sec, second);
+inc0Btn(min0UpBtn, min, minute);
+
+inc1Btn(sec1UpBtn, sec, second);
+inc1Btn(min1UpBtn, min, minute);
